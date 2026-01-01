38669cf2c49244f9ceb9fbfe5f5a08ab4dcc79ab
JatinRoopak <jatin.roopak.xb.8195@gmail.com>
2026-01-02 03:11:03 +0530

    The camera reader node can tell global coordinate

--- sasc/src/camera_reader.cpp
+++ sasc/src/camera_reader.cpp
@@ -3,65 +3,128 @@
 #include "cv_bridge/cv_bridge.h"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/aruco.hpp"
+#include "opencv2/calib3d.hpp"
+#include "sensor_msgs/msg/camera_info.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 using namespace std;
 
 class CameraReader : public rclcpp::Node {
     public:
         CameraReader() : Node("camera_reader_node"){
-            subscription_ = this -> create_subscription<sensor_msgs::msg::Image>(
+
+            tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+            tf_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
+
+            image_sub = this -> create_subscription<sensor_msgs::msg::Image>(
                 "/wrist_camera/image_raw",
                 10,
-                std::bind(&CameraReader::topic_callback, this, std::placeholders::_1)
+                std::bind(&CameraReader::image_callback, this, std::placeholders::_1)
+            );
+
+            info_sub = this->create_subscription<sensor_msgs::msg::CameraInfo>(
+                "/wrist_camera/camera_info",
+                10,
+                std::bind(&CameraReader::info_callback, this, std::placeholders::_1)
             );
 
             RCLCPP_INFO(this-> get_logger(), "waiting for camera data....");
         }
 
     private:
-        void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const {
+
+        cv::Mat camera_matrix;
+        cv::Mat dist_coeff;
+        bool info_received_ = false;
+
+        std::shared_ptr<tf2_ros::TransformListener>tf_listener{nullptr};
+        std::unique_ptr<tf2_ros::Buffer>tf_buffer;
+        
+        void image_callback(const sensor_msgs::msg::Image::SharedPtr msg) const {
+            if (!info_received_){
+                RCLCPP_WARN(this->get_logger(), "Waiting for callibration...");
+                return;
+            }
+
             cv_bridge::CvImagePtr cv_ptr;
+
             try{
                 cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-
-            }catch (cv_bridge::Exception& e) {
+            }catch (cv_bridge::Exception& e){
                 RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
                 return;
             }
 
-            auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
-            auto parameters = cv::aruco::DetectorParameters::create();
+            auto dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+            auto params = cv::aruco::DetectorParameters::create();
 
-            std::vector<int> markerIds;
-            std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+            std::vector<int> ids;
+            std::vector<std::vector<cv::Point2f>> corners;
 
-            cv::aruco::detectMarkers(cv_ptr->image, dictionary, markerCorners, markerIds, parameters);
+            cv::aruco::detectMarkers(cv_ptr->image, dict, corners, ids, params);
 
-            if (markerIds.size()>0) {
-                cv::aruco::drawDetectedMarkers(cv_ptr->image, markerCorners, markerIds);
-                RCLCPP_INFO(this->get_logger(), "Detected marker ID: %d", markerIds[0]);
-                
-                // float x = markerCorners[0][0].x;
-                // float y = markerCorners[0][0].y;
-                // RCLCPP_INFO(this->get_logger(), "Corner 1 Position: x=%.2f, y=%.2f", x, y);
+            if (ids.size() > 0) {
+                std::vector<cv::Vec3d> rvecs, tvecs;
+
+                cv::aruco::estimatePoseSingleMarkers(corners, 0.20, camera_matrix, dist_coeff, rvecs, tvecs);
+
+                for (size_t i = 0; i<ids.size(); i++){
+                    cv::aruco::drawDetectedMarkers(cv_ptr->image, corners, ids);
+                    cv::drawFrameAxes(cv_ptr->image, camera_matrix, dist_coeff, rvecs[i], tvecs[i], 0.1);
+
+                    geometry_msgs::msg::PoseStamped local_pose;
+                    local_pose.header.frame_id = "camera_link_optical";
+                    local_pose.header.stamp = msg->header.stamp;
+
+                    local_pose.pose.position.x = tvecs[i][0];
+                    local_pose.pose.position.y = tvecs[i][1];
+                    local_pose.pose.position.z = tvecs[i][2];
+
+                    local_pose.pose.orientation.w = 1.0;
+
+                    try{
+                        if (tf_buffer->canTransform("base_link", "camera_link_optical", msg->header.stamp, rclcpp::Duration::from_seconds(0.1))){
+                            geometry_msgs::msg::PoseStamped global_pose;
+                            tf_buffer->transform(local_pose, global_pose, "base_link");
+
+                            RCLCPP_INFO(this->get_logger(), "Global pose found X: %.2f, Y: %.2f, Z: %.2f", global_pose.pose.position.x, global_pose.pose.position.y, global_pose.pose.position.z);
+                            
+
+                        }
+                    } catch (tf2::TransformException &ex){
+                        RCLCPP_WARN(this->get_logger(), "Transform Error: %s", ex.what());
+                    }
+
+                    // RCLCPP_INFO(this->get_logger(), "Marker %d is %.3f meters away", ids[i], tvecs[i][2]);
+                }
 
             }
 
-            cv::imshow("Robot Eye", cv_ptr->image);
+            cv::imshow("3D Robot Vision", cv_ptr->image);
             cv::waitKey(1);
 
-            // int height = cv_ptr->image.rows;
-            // int width = cv_ptr->image.cols;
+        }
 
-            // static int count = 0;
-            // if (count%30 == 0){
-            //     RCLCPP_INFO(this->get_logger(), "Received Image: %dx%d pixels", width, height);
+        void info_callback(const sensor_msgs::msg::CameraInfo::SharedPtr msg){
+            if (info_received_) return;
 
-            // }
-            // count++;
+            camera_matrix = cv::Mat(3, 3, CV_64F);
+
+            for (int i=0; i<9; i++) {
+                camera_matrix.at<double>(i/3, i%3) = msg->k[i];
+            }
+
+            dist_coeff = cv::Mat(msg->d).clone();
+
+            info_received_ =  true;
+            RCLCPP_INFO(this->get_logger(), "Calibration Recieved fx=%.2f cx=%.2f", camera_matrix.at<double>(0,0), camera_matrix.at<double>(0,2));
         }
         
-        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;   
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub;   
+        rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr info_sub;
 };
 
 
